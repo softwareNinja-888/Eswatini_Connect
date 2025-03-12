9b177da39c4fa6942175090b70d66470919b5234
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-03-12 13:27:37 +0200

    Refactor code

--- eswatini_connect/src/components/Helper/FormFooter.jsx
+++ eswatini_connect/src/components/Helper/FormFooter.jsx
@@ -107,6 +107,7 @@ export function FormFooter() {
 
                     {/* SUBMIT BUTTON */}
                     <button
+                        aria-label="Submit Form"
                         id="submit_form"
                         type="submit"
                         className="w-full mt-3 bg-black border border-yellowCustom text-white text-lg font-bold py-2 font-lora cursor-pointer flex justify-center items-center gap-2 transition duration-500 hover:scale-103 hover:bg-yellowCustom hover:text-black"
--- eswatini_connect/src/components/Helper/Whatsapp.jsx
+++ eswatini_connect/src/components/Helper/Whatsapp.jsx
@@ -23,6 +23,8 @@ export function Whatsapp() {
 
   return (
     <button
+      aria-label="Send us message on whatsapp"
+      id="Whatsapp"
       onClick={scrollToTop}
       className={`cursor-pointer fixed lg:hidden bottom-5 right-5 p-3 rounded bg-white text-green-500 shadow-lg transition-opacity duration-300 ${
         isVisible ? "opacity-100" : "opacity-0"
