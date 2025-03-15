1ea156b98282a451d430e69337869eb77f10f1e4
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-03-15 12:56:06 +0200

    Add different theme color for hero section

--- eswatini_connect/src/components/Helper/GradientTxt.jsx
+++ eswatini_connect/src/components/Helper/GradientTxt.jsx
@@ -1,7 +1,7 @@
 export function GradientText({text,size='text-5xl',width='w-60',h=""}) {
     return (
       <div className={`flex items-center justify-start ${width}`}>
-        <h1 className={`${size} font-inter900 bg-gradient-to-r from-white to-yellowCustom bg-clip-text text-transparent bg-red-300 ${h}`}>
+        <h1 className={`${size} font-inter900 bg-gradient-to-r from-white to-greenPrimary bg-clip-text text-transparent bg-red-300 ${h}`}>
           {text}
         </h1>
       </div>
--- eswatini_connect/src/components/Hero.jsx
+++ eswatini_connect/src/components/Hero.jsx
@@ -42,9 +42,9 @@ export function Hero(){
                     </div>
 
                     {/* ADD ACTION AFTER CLICKING BUTTON */}
-                    <NavLink to='/contact' className="group flex justify-center items-center gap-3 border border-yellowCustom py-5 w-72 cursor-pointer hover:bg-yellowCustom hover:text-black transition duration-400 ease-in-out">
+                    <NavLink to='/contact' className="group flex justify-center items-center gap-3 border border-greenPrimary py-5 w-72 cursor-pointer hover:bg-yellowCustom hover:text-black transition duration-400 ease-in-out">
                         <div className="font-inter700">Get a Free Consultaion</div>
-                        <FaArrowRight size={24}  className="text-yellowCustom group-hover:text-black"/>
+                        <FaArrowRight size={24}  className="text-greenPrimary group-hover:text-black"/>
                     </NavLink>
                 </motion.div>
 
